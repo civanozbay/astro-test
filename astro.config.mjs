81301404fd71f4ed3244dd79a57dea18ceb99aac
civanozbay <civan.ozbay@coachhub.com>
2026-01-27 13:21:27 +0100

    icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,19 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
+import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
     site: "https://civanozbay.github.io",
     base: "/astro-test",
+    vite: {
+    resolve: {
+      alias: {
+        "@": "/src",
+      },
+    },
+    plugins: [tailwindcss()],
+    optimizeDeps: {
+      exclude: ["@resvg/resvg-js"],
+    },
+  }
 });
