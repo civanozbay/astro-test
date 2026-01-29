e85fb2234ddd807475f6f6139fbbe863b9b4f1c0
civanozbay <civan.ozbay@coachhub.com>
2026-01-29 13:39:15 +0100

    about

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,19 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
+import mdx from "@astrojs/mdx";
 // https://astro.build/config
 export default defineConfig({
     site: "https://civanozbay.github.io",
     base: "/astro-test",
+    markdown: {
+    shikiConfig: {
+      // For more themes, visit https://shiki.style/themes
+      themes: { light: "min-light", dark: "night-owl" },
+      wrap: true,
+    },
+  },
+    integrations:[mdx()],
     vite: {
     resolve: {
       alias: {
